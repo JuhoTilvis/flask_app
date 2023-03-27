b199904b5935addbb5fbcfc4f6fc09ba65e8b66e
JuhoTilvis <128355278+JuhoTilvis@users.noreply.github.com>
2023-03-27 11:42:49 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import json
 
 app = Flask(__name__)
 
@@ -13,13 +14,15 @@ paivat=['Maanantai','Tiistai','Keskiviikko', 'Torstai','Perjantai','Lauantai','S
 
 @app.route('/api', methods= ['GET'])
 
+
 def index():
     return render_template("mittaukset.html", taulukko=lampotilat, paivat=paivat)
 
-@app.route('\lisaa', methods= ['POST'])
+@app.route('/lisaa', methods= ['POST'])
 def lisaa():
     uusimittaus = request.get_json(force=True)
     lampotilat.append(uusimittaus)
+    return(json.dumps(uusimittaus))
 
 if __name__ == "__main__":
     app.run(debug=True)
