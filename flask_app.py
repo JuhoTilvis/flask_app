e646ebf9f2d63a8eeb6cb577640b82ba1468d357
JuhoTilvis <128355278+JuhoTilvis@users.noreply.github.com>
2023-04-03 09:10:59 +0300

    muutokset

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 con = sqlite3.connect("mittaukset.dbe3")
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXIST mittaukset (id INTEGER PRIMARY KEY, paiva INTEGER, mittaus INTEGER)")
+cur.execute("CREATE TABLE IF NOT EXISTS mittaukset (id INTEGER PRIMARY KEY, paiva INTEGER, mittaus INTEGER)")
 con.commit()
 con.close()
 
@@ -24,8 +24,17 @@ paivat=['Maanantai','Tiistai','Keskiviikko', 'Torstai','Perjantai','Lauantai','S
 def index():
     return render_template("mittaukset.html", taulukko=lampotilat, paivat=paivat)
 
-@app.route('/lisaa', methods= ['POST'])
-def lisaa():
+@app.route('/lisaakantaan', methods= ['POST'])
+def lisaa_tietokantaan():
+
+    uusimittaus = request.json(force=True
+                               )
+    con = sqlite3.connect("mittaukset.dbe3")
+    cur = con.cursor()
+    cur.execute("INSERT INTO mittaukset (paiva,mittaus) VALUES(?,?)", [uusimittaus["x"], uusimittaus["y"]])
+    con.commit()
+    con.close()
+
     uusimittaus = request.get_json(force=True)
     lampotilat.append(uusimittaus)
     return(json.dumps(uusimittaus))
